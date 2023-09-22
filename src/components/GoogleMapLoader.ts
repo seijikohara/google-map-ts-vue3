1a950d9b57837e67403a0a15b05a18a9b98ee522
Seiji Kohara <sage.0118@gmail.com>
2023-09-22 11:41:05 +0900

    Refactoring

--- src/components/GoogleMapLoader.ts
+++ src/components/GoogleMapLoader.ts
@@ -1,47 +1,47 @@
-import { stringify } from "query-string";
-
-type GoogleMapsScriptHandler = () => void;
+type GoogleMapsScriptHandler = () => void
 
 interface GoogleMapWindow extends Window {
-  handleLoadGoogleMapsScript: GoogleMapsScriptHandler;
-  google: unknown;
+  handleLoadGoogleMapsScript: GoogleMapsScriptHandler
+  google: unknown
 }
-declare const window: GoogleMapWindow;
+declare const window: GoogleMapWindow
 
 export type GoogleMapsScriptLoadParams = {
-  key: string;
-  libraries: string;
-};
+  key: string
+  libraries: string
+}
 
-export function loadGoogleMapsScript(
-  params: GoogleMapsScriptLoadParams
-): Promise<void> {
+export const loadGoogleMapsScript = (params: GoogleMapsScriptLoadParams): Promise<void> => {
   return new Promise((resolve, reject) => {
-    console.info("[GoogleMap] Script loading.");
+    console.info('[GoogleMap] Script loading.')
     if (window.google) {
-      console.info("[GoogleMap] Script already loaded.");
-      return resolve();
+      console.info('[GoogleMap] Script already loaded.')
+      return resolve()
     }
 
-    const headElement = document.querySelector("head");
+    const headElement = document.querySelector('head')
     if (!headElement) {
-      return reject(new Error("[GoogleMap] Head node is undefined."));
+      return reject(new Error('[GoogleMap] Head node is undefined.'))
     }
 
-    const scriptElement = document.createElement("script");
-    scriptElement.src = `https://maps.googleapis.com/maps/api/js?${stringify({
-      callback: "handleLoadGoogleMapsScript",
-      ...params,
-    })}`;
-    headElement.appendChild(scriptElement);
+    const scriptElement = document.createElement('script')
+    const url = new URL('https://maps.googleapis.com/maps/api/js')
+    Object.entries({
+      callback: 'handleLoadGoogleMapsScript',
+      ...params
+    }).forEach(([key, value]) => url.searchParams.set(key, value))
+    scriptElement.src = url.toString()
+    headElement.appendChild(scriptElement)
+
     window.handleLoadGoogleMapsScript = () => {
-      console.info("[GoogleMap] Script loaded.");
-      resolve();
-    };
+      console.info('[GoogleMap] Script loaded.')
+      resolve()
+    }
+
     setTimeout(() => {
       if (!window.google) {
-        reject(new Error("[GoogleMap] Failed load google api"));
+        reject(new Error('[GoogleMap] Failed to load google api'))
       }
-    }, 5000);
-  });
+    }, 5000)
+  })
 }
