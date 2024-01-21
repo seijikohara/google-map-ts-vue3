f4e64eaf92bb12cd27897218f15936eb097ecf62
Seiji Kohara <sage.0118@gmail.com>
2024-01-21 22:49:03 +0900

    Improve Compilation from TypeScript to JavaScript

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,33 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+
+const name = 'google-map-ts-vue3'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    lib: {
+      entry: 'src/components/index.ts',
+      name,
+      fileName: (format) => `${name}.${format}.js`
+    },
+    sourcemap: true,
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
 })
